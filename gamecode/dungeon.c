e048ad43822d7a0675d27daff37e80ca41a87683
ryantullis <tulliscryan@gmail.com>
2021-02-25 15:42:29 -0600

    added placeholder for rendering tun_dist_map

--- gamecode/dungeon.c
+++ gamecode/dungeon.c
@@ -25,6 +25,7 @@ typedef struct file_info {
   int load;
   int save;
   int ren_non_tun_dist_map;
+  int ren_tun_dist_map;
 } file_info_t;
 
 typedef struct corridor_path {
@@ -812,7 +813,11 @@ void render_dungeon(dungeon_t *d, file_info_t *f)
 
     //render the non-tunneling distance map if specified (1.03 defaults to true)
     if (f->ren_non_tun_dist_map){
-      
+      for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+    for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+    }
+    if (f->ren_tun_dist_map){
+
     }
   }
 }
