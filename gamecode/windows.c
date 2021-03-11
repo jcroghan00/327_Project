9ec006071551eda9a1239a9bb11c1c619e12a6ec
Ryan <tulliscryan@gmail.com>
2021-03-11 17:56:38 -0600

    created windows.c and .h

---
+++ gamecode/windows.c
@@ -0,0 +1,13 @@
+#include <ncurses.h>
+
+#include "dungeon.h"
+#include "windows.h"
+
+WINDOW create_window(){
+new_win = create_newwin(DUNGEON_Y, DUNGEON_X-50, 0 , 25)
+    return new_win;
+}
+
+void create_monster_list(){
+    
+}
---
+++ gamecode/windows.h
@@ -0,0 +1,8 @@
+#ifndef GAMECODE_WINDOWS_H
+#define GAMECODE_WINDOWS_H
+
+#include <ncurses.h>
+
+WINDOW monster_list;
+
+#endif
