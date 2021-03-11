d8f7d4f13f9f25a45176a44bdea3f20472c49972
Ryan <tulliscryan@gmail.com>
2021-03-11 16:20:25 -0600

    added all reserved keys to switch statment

--- gamecode/pc.c
+++ gamecode/pc.c
@@ -35,109 +35,182 @@ void move_pc_ncurses(dungeon_t *d){
     int x = 0; 
     int y = 0;
 
-    char val  = getch();
+    int val  = getch();
 
     switch (val)
     {
-    case '9':
-    case 'u':
-        y = -1;
-        x = 1;
-        /* code */
-        break;
-    case '7':
-    case 'y':
-        y = 0;
-        x = -1;
-        /* code */
-        break;
-    case '8':
-    case 'k':
-        y = -1;
-        x = 0;
-        /* code */
-        break;
-    case '6':
-    case 'l':
-        y = 0;
-        x = 1;
-        /* code */
-        break;
-    case '3':
-    case 'n':
-        y = 1;
-        x = 1;
-        /* code */
-        break;
-    case '2':
-    case 'j':
-        y = 1;
-        x = 0;
-        /* code */
-        break;
-    case '1':
-    case 'b':
-        y = 1;
-        x = -1;
-        /* code */
-        break;
-    case '4':
-    case 'h':
-        y = 0;
-        x = -1;
-        /* code */
-        break;
-    case '>':
-        if(d->map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] == ter_stairs_up){
-            delete_dungeon(d);
-            d->num_monsters = 7;
-            init_dungeon(d);
-            gen_dungeon(d);
-            clear();
-            render_ncurses(d);
-            refresh();
-        }
+        // Move up-left
+        case KEY_HOME:
+        case '7':
+        case 'y':
+            y = 0; x = -1; break;
 
-        break;
-    case '<':
-        if(d->map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] == ter_stairs_down){
-            delete_dungeon(d);
-            d->num_monsters = 7;
-            init_dungeon(d);
-            gen_dungeon(d);
-            clear();
-            render_ncurses(d);
-            refresh();
-        }
+        // Move up
+        case KEY_UP:
+        case '8':
+        case 'k':
+            y = -1; x = 0; break;
 
-        break;
-    case '5':
-    case ' ':
-    case '.':
-        x = 0;
-        y = 0;
-        break;
-    case 'Q':
-        endwin();
-        delete_dungeon(d);
-        exit(0);
-    default:
-        x = 0;
-        y = 0;
-        break;
+        // Move up-right
+        case KEY_PPAGE:
+        case '9':
+        case 'u':
+            y = -1; x = 1; break;
+
+        // Move right
+        case KEY_RIGHT:
+        case '6':
+        case 'l':
+            y = 0; x = 1; break;
+
+        // Move down-right
+        case KEY_NPAGE:
+        case '3':
+        case 'n':
+            y = 1; x = 1; break;
+
+        // Move down
+        case KEY_DOWN:
+        case '2':
+        case 'j':
+            y = 1; x = 0; break;
+
+        // Move down-left
+        case KEY_END:
+        case '1':
+        case 'b':
+            y = 1; x = -1; break;
+
+        // Move left
+        case KEY_LEFT:
+        case '4':
+        case 'h':
+            y = 0; x = -1; break;
+
+        // rest
+        case KEY_B2:
+        case ' ':
+        case '.':
+        case '5':
+            y = 0; x = 0; break;
+
+        // Go down stairs
+        case '>':
+            if(d->map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] == ter_stairs_up){
+                delete_dungeon(d);
+                d->num_monsters = 7;
+                init_dungeon(d);
+                gen_dungeon(d);
+                clear();
+                render_ncurses(d);
+                refresh();
+            }
+            break;
+
+        // go up stairs
+        case '<':
+            if(d->map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] == ter_stairs_down){
+                delete_dungeon(d);
+                d->num_monsters = 7;
+                init_dungeon(d);
+                gen_dungeon(d);
+                clear();
+                render_ncurses(d);
+                refresh();
+            }
+            break;
+
+        //TODO Display character information
+        case 'c':
+            break;
+
+        //TODO Drop item
+        case 'd':
+            break;
+
+        //TODO Display equipment
+        case 'e':
+            break;
+
+        //TODO Toggle fog-of-war
+        case 'f':
+            break;
+
+        //TODO Teleport (goto)
+        case 'g':
+            break;
+
+        //TODO Display inventory
+        case 'i':
+            break;
+
+        //TODO Display monster list
+        case 'm':
+            break;
+
+        //TODO Display the default (terrain) map
+        case 's':
+            break;
+
+        //TODO Take off item
+        case 't':
+            break;
+
+        //TODO Wear item
+        case 'w':
+            break;
+
+        //TODO Expunge item
+        case 'x':
+            break;
+
+        //TODO Display the non-tunneling distance map
+        case 'D':
+            break;
+
+        //TODO Inspect equipped item
+        case 'E':
+            break;
+
+        //TODO Display the hardness map
+        case 'H':
+            break;
+
+        //TODO Inspect inventory item
+        case 'I':
+            break;
+
+        //TODO Look at monster
+        case 'L':
+            break;
+
+        // Quit the game
+        case 'Q':
+            endwin();
+            delete_dungeon(d);
+            exit(0);
+
+        //TODO Display the tunneling distance map
+        case 'T':
+            break;
+
+        default:
+            x = 0;
+            y = 0;
+            break;
     }
 
-        if(d->map[d->pc->pos[dim_y] + y][d->pc->pos[dim_x] + x] == ter_wall ||
-           d->map[d->pc->pos[dim_y] + y][d->pc->pos[dim_x] + x] == ter_wall_immutable)
-        {y = 0, x = 0;}
+    if(d->map[d->pc->pos[dim_y] + y][d->pc->pos[dim_x] + x] == ter_wall ||
+       d->map[d->pc->pos[dim_y] + y][d->pc->pos[dim_x] + x] == ter_wall_immutable)
+    {y = 0, x = 0;}
 
 
 
-      d->character_map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] = NULL;
-        d->pc->pos[dim_y] += y;
-        d->pc->pos[dim_x] += x;
-        d->character_map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] = d->pc;
-    }
+    d->character_map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] = NULL;
+    d->pc->pos[dim_y] += y;
+    d->pc->pos[dim_x] += x;
+    d->character_map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] = d->pc;
+}
 
 int pc_next_pos(dungeon_t *d)
 {
