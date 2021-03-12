a14532e55bba1420868e992e776c7cd7de7cc36a
Jack Croghan <jcroghan00@gmail.com>
2021-03-11 18:15:10 -0600

    better merge

--- gamecode/pc.c
+++ gamecode/pc.c
@@ -248,43 +248,10 @@ void move_pc_ncurses(dungeon_t *d, heap_t *h)
             mvprintw(0, 0, "Invalid Key!");
             move_pc_ncurses(d, h);
             break;
-
-<<<<<<< HEAD
-
-int pc_next_pos(dungeon_t *d)
-{
-    int hasMoved = 0;
-    while(!hasMoved)
-    {
-        int dx = (rand() % 3) - 1;
-        int dy = (rand() % 3) - 1;
-
-   
-        if(d->map[d->pc->pos[dim_y] + dy][d->pc->pos[dim_x] + dx] == ter_wall || d->map[d->pc->pos[dim_y] + dy][d->pc->pos[dim_x] + dx] == ter_wall_immutable){continue;}
-
-
-        if (d->character_map[d->pc->pos[dim_y] + dy][d->pc->pos[dim_x] + dx] != NULL &&
-            d->character_map[d->pc->pos[dim_y] + dy][d->pc->pos[dim_x] + dx] != d->pc)
-        {
-            d->character_map[d->pc->pos[dim_y] + dy][d->pc->pos[dim_x] + dx]->living = 0;
-            d->num_monsters--;
-        }
-        d->character_map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] = NULL;
-        d->pc->pos[dim_y] += dy;
-        d->pc->pos[dim_x] += dx;
-        d->character_map[d->pc->pos[dim_y]][d->pc->pos[dim_x]] = d->pc;
-        //update monster path making
-        dijkstra_non_tunneling(d);
-        dijkstra_tunneling(d);
-
-        hasMoved = 1;
-        
-=======
         default:
             mvprintw(0, 0, "Invalid Key!");
             move_pc_ncurses(d, h);
             break;
->>>>>>> 0aac82257dd7a4411886f0b5449ab78325470834
     }
     render_ncurses(d);
     refresh();
