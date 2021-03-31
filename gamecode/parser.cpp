32af1fd68e10465ba3598527eeb69cec90d333d9
Jack Croghan <jcroghan00@gmail.com>
2021-03-31 17:16:01 -0500

    counting monster types

--- gamecode/parser.cpp
+++ gamecode/parser.cpp
@@ -7,10 +7,31 @@
 
 using namespace std;
 
+int count_types(std::string filepath, std::string delimiter)
+{
+    int num_types = 0;
+    ifstream inFile(filename);
+
+    std::string line;
+    while(getline(inFile, line))
+    {
+        if(!line.compare(delimiter)){
+            ++num_types;
+        }
+    }
+
+    inFile.close();
+    return num_types;
+}
+
 void monster_parser(){
     char *filepath = (char *)malloc(sizeof(getenv("HOME")) + sizeof("/.rlg327/monster_desc.txt"));
     strcat(filepath, getenv("HOME"));
     strcat(filepath, "/.rlg327/monster_desc.txt");
+
+    int num_monster_types = count_types(filepath, "BEGIN MONSTER");
+    cout << num_monster_types << endl;
+
     ifstream inFile(filepath);
 
     if(!inFile){
@@ -20,13 +41,20 @@ void monster_parser(){
 
     std::string metadata;
     getline(inFile, metadata);
-    cout << metadata << endl;
-
-    if(!metadata.compare("RLG327 MONSTER DESCRIPTION 1\n")){
+    if(metadata.compare("RLG327 MONSTER DESCRIPTION 1")){
         inFile.close();
         return;
     }
 
+    cout << metadata << endl;
+
+    /*
+    std::string line;
+    while(getline(inFile, line))
+    {
+        cout << line << endl;
+    }
+     */
     inFile.close();
 }
 void item_parser(){
